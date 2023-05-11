195e710ef7c34524fc69a9c0dbab7db73a678699
Conte-Riesling <130985551+Conte-Riesling@users.noreply.github.com>
2023-05-11 22:17:19 +0300

    NEW update group_project.py

--- group_project.py
+++ group_project.py
@@ -1,7 +1,7 @@
-a, b, c = list(map(int, input('Enter three numbers, separated by spaces: ').split()))  # reading data
+a,b,c = int(input()),int(input()),int(input())
 
-minimum, middle, maximum = min(a, b, c), a + b + c - min(a, b, c) - max(a, b, c), max(a, b, c)  # initialization min, mid, max
-# output new data
-print(f'''Minimum one: {minimum}.
-Middle one: {middle}.
-Maximum one: {maximum}.''')
+minimum = min(a, b, c)
+maximum = max(a, b, c)
+middle = a + b + c - minimum - maximum
+
+print(maximum, middle, minimum)
