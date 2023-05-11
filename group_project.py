c89d5e4dfad57b0d2103414a0731cb84097a580e
Daria Kowalewa <kowalewa.daria2012@gmail.com>
2023-05-11 17:05:40 +0300

    Some new

--- group_project.py
+++ group_project.py
@@ -1,10 +1,7 @@
-a = int(input())
-b = int(input())
-c = int(input())
+# Можно использовать функцию map вместо вызывания 3 отдельных функций
+a, b, c = map(int, input().split())
+minimum, middle, maximum = sorted([a, b, c])
 
-minimum = min(a, b, c)
-maximum = max(a, b, c)
-middle = a + b + c - minimum - maximum
 
 print(maximum)
 print(middle)
