be043efbee80df4a56a5fa41c3fe3309a2dd63be
finchf <130279835+finchf@users.noreply.github.com>
2023-05-11 19:06:38 +0300

    Update group_project.py

--- group_project.py
+++ group_project.py
@@ -1,4 +1,3 @@
-#зміни
 a, b, c = list(map(int, input('Enter three numbers, separated by spaces: ').split()))  # reading data
 
 minimum, middle, maximum = min(a, b, c), a + b + c - min(a, b, c) - max(a, b, c), max(a, b, c)  # initialization min, mid, max
