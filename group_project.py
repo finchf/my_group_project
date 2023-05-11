74236740fa4edf5a0eab7e61bd75986cef4da359
finchf <130279835+finchf@users.noreply.github.com>
2023-05-11 20:58:23 +0300

    Update group_project.py

--- group_project.py
+++ group_project.py
@@ -1,13 +1,4 @@
 
-# Можно использовать функцию map вместо вызывания 3 отдельных функций
-a, b, c = map(int, input().split())
-minimum, middle, maximum = sorted([a, b, c])
-
-
-print(maximum)
-print(middle)
-print(minimum)
-=======
 a, b, c = list(map(int, input('Enter three numbers, separated by spaces: ').split()))  # reading data
 
 minimum, middle, maximum = min(a, b, c), a + b + c - min(a, b, c) - max(a, b, c), max(a, b, c)  # initialization min, mid, max
